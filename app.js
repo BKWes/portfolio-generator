1b3e908d02129a72c20086b16d79f2c44492ae0d
Weston <barnesweston@gmail.com>
2022-03-29 17:10:01 -0500

    assign user inputs to variables, write function to generate a webpage using user input variables, modularize code

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require('fs');
 
-const printProfileData = profileDataArr => {
-    //for loop is same as forEach method
-    for (let i=0; i<profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
+const generatePage = require('./src/page-template.js');
 
-    console.log('============');
-// same as for loop above forEach is an ARRAY METHOD - only works on arrays
-    profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
+const profileDataArgs = process.argv.slice(2);
 
-printProfileData(profileDataArgs);
+const [userName, github] = profileDataArgs;
+
+fs.writeFile('index.html', generatePage(userName, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+// const printProfileData = profileDataArr => {
+//     //for loop is same as forEach method
+//     for (let i=0; i<profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('============');
+// // same as for loop above forEach is an ARRAY METHOD - only works on arrays
+//     profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
