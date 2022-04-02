c3893c19e78d70d00c7331547e9e1a21dabdc840
Weston <barnesweston@gmail.com>
2022-04-02 16:28:56 -0500

    add stylesheet and functionality to copy file to distribution

--- app.js
+++ app.js
@@ -128,9 +128,13 @@ promptUser()
   .then(portfolioData => {
     const pageHTML = generatePage(portfolioData);
 
-    fs.writeFile('./index.html', pageHTML, err => {
+    fs.writeFile('./dist/index.html', pageHTML, err => {
         if (err) throw err;
-
-        console.log('Portfolio complete! Check out index.html to see the output!');
+        console.log('Page created! Check out index.html in this directory to see the output!');
+        
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) throw err;
+            console.log('Style sheet copied successfully!');
+        });
     });
   });
